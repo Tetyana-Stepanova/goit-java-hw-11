7aa368c542f6b4d804d99ae37c602ce0fb590be7
Tetyana-Stepanova <95179655+Tetyana-Stepanova@users.noreply.github.com>
2022-02-14 22:54:42 +0200

    Create Threads.java

---
+++ Threads.java
@@ -0,0 +1,67 @@
+public class Threads {
+    public static void main (String [] args) throws InterruptedException{
+        Chronometer start=new Chronometer();
+        Messenger t1=new Messenger(5, start);
+        Messenger t2=new Messenger(1, start);
+        t1.countTime=10;
+        t1.isEverySecond=true;
+        t2.countTime=10;
+        t2.isEverySecond=false;
+        new Thread(t1, "t1").start();
+        new Thread(t2, "t2").start();
+        start.countTime(t1, t2, 10);
+    }
+}
+
+class Chronometer {
+    public int time=0;
+    public void countTime (Messenger m, Messenger m1, int period) {
+        for (int i=0; i<period; i++){
+            synchronized(this) {
+                time++;
+                System.out.println(time);
+                if (i==period-1) Messenger.finish=true;
+                m.flag=false;
+                this.notify();
+                m1.flag=false;
+                this.notify();
+            }
+            try {   Thread.sleep(1000);}
+            catch (InterruptedException e) {};
+        }
+    }
+}
+class Messenger implements Runnable{
+    private int time;
+    public Chronometer ch;
+    public static boolean finish=false;
+    public boolean flag=true;
+    public int countTime;
+    public boolean isEverySecond;
+    Messenger(int time, Chronometer ch) {
+        this.time=time;
+        this.ch=ch;
+    }
+
+   public void waitForTime() {
+       while (true) {
+           synchronized (ch) {
+               try {
+                   if (isEverySecond && ch.time % 1 == 0)
+                       System.out.println("Thread at intervals 1");
+                   if(!isEverySecond && ch.time % 5 ==0)
+                       System.out.println("5 seconds have passed");
+                   ch.wait();
+                   if (countTime == ch.time) return;
+               } catch (InterruptedException e) {
+               }
+
+           }
+       }
+   }
+
+    public void run()  {
+        waitForTime();
+        System.out.println("The end");
+    }
+}
